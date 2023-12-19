105f7f53fbb41009f93e1aa82dca219ac36ef27a
Matt Shenton <mshenton75@gmail.com>
2023-12-18 22:21:55 -0500

    Day 6 Part 2

---
+++ 2022/src/Day5/Part2.purs
@@ -0,0 +1,35 @@
+module Day5.Part2
+  ( solution
+  )
+  where
+
+import Prelude
+
+import Data.Array as Array
+import Data.Set as Set
+import Data.String.CodeUnits (toCharArray)
+import Effect.Unsafe (unsafePerformEffect)
+import Node.Encoding (Encoding(..))
+import Node.FS.Sync (readTextFile)
+import Types (File(..))
+
+
+solution :: String 
+solution = show do 
+  let datastream = parseFile readFile
+  numCharsBeforeKey datastream
+
+readFile :: File
+readFile = File $ unsafePerformEffect $ readTextFile ASCII "src/Day5/Input.txt"
+
+parseFile :: File -> Array Char 
+parseFile (File file) = toCharArray file
+
+numCharsBeforeKey :: Array Char -> Int 
+numCharsBeforeKey chars = processKeys 0
+
+  where 
+    processKeys idx
+      | 14 <- Set.size $ Set.fromFoldable $ Array.slice (idx - 14) idx chars = idx 
+      | otherwise = processKeys (idx + 1)
+
