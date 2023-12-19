eee2820efaef842f5d123e9547aba27db5b68186
Matt Shenton <mshenton75@gmail.com>
2023-12-18 22:19:28 -0500

    Day 6 Part 1

--- 2022/src/Main.purs
+++ 2022/src/Main.purs
@@ -10,6 +10,7 @@ import Day2.Part2 as Day2Part2
 import Day3.Part1 as Day3Part1
 import Day4.Part1 as Day4Part1
 import Day4.Part2 as Day4Part2
+import Day5.Part1 as Day5Part1
 import Effect (Effect)
 import Effect.Class.Console (error)
 import Effect.Console (log)
@@ -27,4 +28,5 @@ main = do
     Just "day-3", Just "part-1" -> log Day3Part1.solution
     Just "day-4", Just "part-1" -> log Day4Part1.solution
     Just "day-4", Just "part-2" -> log Day4Part2.solution
+    Just "day-5", Just "part-1" -> log Day5Part1.solution
     _, _ -> error "Unknown arguments"
