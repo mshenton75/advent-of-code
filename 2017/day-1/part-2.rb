74ce1b6c135618066aff61d27f36f45ffa71224e
Matt Shenton <mshenton75@gmail.com>
2024-05-28 14:25:26 -0400

    Refactor

--- 2017/day-1/part-2.rb
+++ 2017/day-1/part-2.rb
@@ -2,10 +2,4 @@ require 'byebug'
 
 data = File.read('input.txt')
 digits = data.chars
-sum = 0
-
-digits.zip(digits.rotate(digits.length / 2)).each do |pair|
-  sum += pair.first.to_i if pair.first == pair.last
-end
-
-puts sum
+puts digits.zip(digits.rotate(digits.length / 2)).select { |pair| pair.first == pair.last }.map { |pair| pair.uniq.first.to_i }.sum
